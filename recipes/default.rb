6f8be4ec9e89d2fecfe08035f885b37d4c6611ca
Shinichi Urabe <s-urabe@oz-vision.co.jp>
2017-10-27 03:32:38 +0900

    fstab の設定から S3 をマウント

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,14 +18,21 @@ end
 
 execute "install goofys" do
     user "root"
-    command <<-EOC
-        export GOPATH=$HOME/go
-        go get github.com/kahing/goofys
-        go install github.com/kahing/goofys
-        S3_MOUNT_DIR=/var/s3
-        S3_BUCKET_NAME=affi-test-s3
-        $HOME/go/bin/goofys --uid=1000 --gid=1000 ${S3_BUCKET_NAME} ${S3_MOUNT_DIR}
-    EOC
+    command 'GOPATH=/root/go go get github.com/kahing/goofys'
+    command 'GOPATH=/root/go go install github.com/kahing/goofys'
+    not_if { File.exists?('/root/go/bin/goofys') }
+end
+
+execute "write fstab" do
+    user "root"
+    command "echo '/root/go/bin/goofys#affi-test-s3 /var/s3/ fuse _netdev,allow_other,--file-mode=0664,--dir-mode=0775,--uid=1000,--gid=1000 0 0' >> /etc/fstab"
+    not_if "cat /etc/fstab | awk '{print $2}' |grep '/var/s3/'"
+end
+
+execute "mount s3" do
+    user "root"
+    command 'mount -a'
+    not_if { Dir.exists?('/var/s3/file') }
 end
 
 directory "/var/s3/file" do
